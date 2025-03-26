6961567a86bd049ef699d04c2e4a3f4864afeac0
Doan Minh Duy <duydoan812@gmail.com>
2025-03-26 22:22:15 +0700

    Finish Commit

---
+++ Static Method_VS/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography;
+
+public class Student
+{
+    private int rollno;
+    private string name;
+    private static string college = "BBDIT";
+
+    public Student(int r, string n) //Constructor
+    {
+        rollno = r;
+        name = n;
+    }
+
+    public static void Change()
+    {
+        college = "CODEGYM";
+    }
+
+    public void Display()
+    {
+        Console.WriteLine(rollno + "" + name + "" + college);
+    }
+}
+
+public class program
+{
+    public static void Main(string[] args)
+    {
+        Student.Change();
+
+        Student s1 = new Student(111, "Hoang"); //s1 la mot bien tham chieu, tham chieu den doi tuong Student(111,"Hoang") duoc luu trong Heap 
+        Student s2 = new Student(222, "Thanh");
+        Student s3 = new Student(333, "Lam");
+        s1.Display();
+        s2.Display();
+        s3.Display();
+    }
+    
+}
